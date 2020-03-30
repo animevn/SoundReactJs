3efd541ea9674f00f7ecf496cd3edbc6e11eca66
animevn <haanhth21@gmail.com>
2020-03-30 14:02:13 +0700

    add dropdown (spinner in Android, kind of picker in iOS)

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "bootstrap/js/dist/dropdown";
 
 function Home() {
 
+  const options = [1, 2, 3, 4, 5];
+  const [state, setState] = useState(1);
+
+  function onButtonClick(event) {
+    event.preventDefault();
+    setState(event.target.value);
+  }
+
+  const buttonOptions = (
+    options.map((value => {
+      return (
+        <button className="btn dropdown-item" onClick={onButtonClick} value={value}>
+          {value}
+        </button>
+      )
+    }))
+  );
+
   return (
     <div className="container mt-3">
       <div className="dropdown d-flex justify-content-around">
-        <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <span className="mx-2">Choose Repeat</span>
+        <button className="btn btn-success dropdown-toggle" data-toggle="dropdown">
+          <span className="mx-2">{"Choose Repeat: " + state}</span>
         </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <button className="btn dropdown-item">1</button>
-          <button className="dropdown-item" >2</button>
-          <button className="dropdown-item" >3</button>
+        <div className="dropdown-menu">
+          {buttonOptions}
         </div>
       </div>
 
